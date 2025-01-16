4a09e4e2419bd719158eb52b8b41efc816f955e3
x2Beef <62670550+TechCraftYT@users.noreply.github.com>
2025-01-16 21:53:55 +1300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Made by x2Beef Coding Productions
+// https://www.github.com/x2beef/drive-master
+
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Get the status and info of drives connected
+        DriveInfo[] drives = DriveInfo.GetDrives();
+
+        foreach (DriveInfo drive in drives)
+        {
+            // Only show drives that are ready to be used eg: C:\
+            if (drive.IsReady)
+            {
+                // Uses WriteLine to display X amount of space and names
+                Console.WriteLine($"Drive {drive.Name}");
+                Console.WriteLine($"Drive Letter: {drive.Name[0]}:");
+                Console.WriteLine($"Total Space: {drive.TotalSize / (1024 * 1024 * 1024)} GB");
+                Console.WriteLine($"Available Space: {drive.AvailableFreeSpace / (1024 * 1024 * 1024)} GB");
+                Console.WriteLine(new string('-', 40));
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
