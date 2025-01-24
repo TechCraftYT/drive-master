be992a925d8695955ffcca94a53d9c8bdf4e471c
x2Beef <62670550+TechCraftYT@users.noreply.github.com>
2025-01-24 22:25:54 +1300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿// Made by x2Beef Coding Productions
+// Made by x2Beef Coding Productions
 // https://www.github.com/x2beef/drive-master
-
 
 using System;
 using System.IO;
+using System.Threading;
 
 class Program
 {
@@ -17,7 +17,11 @@ class Program
             // Only show drives that are ready to be used eg: C:\
             if (drive.IsReady)
             {
-                // Uses WriteLine to display X amount of space and names
+                Console.WriteLine("Made by x2Beef Coding Productions");
+                Thread.Sleep(4000);
+                Console.WriteLine("Introducing Drive-Master 1.0.1");
+
+                // Uses WriteLine to display drive info
                 Console.WriteLine($"Drive {drive.Name}");
                 Console.WriteLine($"Drive Letter: {drive.Name[0]}:");
                 Console.WriteLine($"Total Space: {drive.TotalSize / (1024 * 1024 * 1024)} GB");
@@ -25,7 +29,6 @@ class Program
                 Console.WriteLine(new string('-', 40));
                 Console.ReadKey();
             }
-
         }
     }
 }
